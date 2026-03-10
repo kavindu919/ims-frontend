c6eed3b87255f2b9a2b81ea9df916d42564b79a9
kavindu919 <jayakodyk2@gmail.com>
2026-03-10 20:00:50 +0530

    feat: create auth interface and schema

---
+++ utils/interfaces/authInterface.ts
@@ -0,0 +1,18 @@
+export interface loginPageInterface {
+  email: string;
+  password: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface UserContextType {
+  user: User | null;
+  loading: boolean;
+  error: string | null;
+  logout: () => Promise<void>;
+  setUser: (user: User | null) => void;
+}
