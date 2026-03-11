b288f602c0672922c08334f5078d3cb1eb6de5dc
kavindu919 <jayakodyk2@gmail.com>
2026-03-11 09:48:17 +0530

    feat: cerate cupboard services

--- services/cupboard.Services.ts
+++ services/cupboard.Services.ts
@@ -1,12 +1,12 @@
 import api from "@/lib/axios";
+import { CupboardQueryProps } from "@/utils/interfaces/cupboardInterface";
 
-export const fetchAllCupboards = async () => {
+export const fetchAllCupboards = async (query: CupboardQueryProps) => {
   try {
-    const res = await api.get("/auth/cupboards");
+    const res = await api.get("/auth/cupboards", { params: query });
     return res.data;
   } catch (error: any) {
-    const errorMessage =
-      error?.message || error?.response?.data?.message || "Login failed";
+    const errorMessage = error?.message || error?.response?.data?.message;
     throw new Error(errorMessage);
   }
 };
@@ -20,8 +20,32 @@ export const createCupboard = async (data: {
     const res = await api.post("/auth/cupboards/create", data);
     return res.data;
   } catch (error: any) {
-    const errorMessage =
-      error?.message || error?.response?.data?.message || "Login failed";
+    const errorMessage = error?.message || error?.response?.data?.message;
+    throw new Error(errorMessage);
+  }
+};
+
+export const deleteCupboard = async (id: number) => {
+  try {
+    const res = await api.post("/auth/cupboard/delete", { id });
+    return res.data;
+  } catch (error: any) {
+    const errorMessage = error?.message || error?.response?.data?.message;
+    throw new Error(errorMessage);
+  }
+};
+
+export const updateCupboard = async (data: {
+  id: number;
+  name: string;
+  location?: string;
+  description?: string;
+}) => {
+  try {
+    const res = await api.post("/auth/cupboard/update", data);
+    return res.data;
+  } catch (error: any) {
+    const errorMessage = error?.message || error?.response?.data?.message;
     throw new Error(errorMessage);
   }
 };
